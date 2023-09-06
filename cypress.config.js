e6bc185f508990a78b6264e16ddd430d3aa2d7c3
DimaStasyshen <130337129+DimaStasyshen@users.noreply.github.com>
2023-09-05 18:56:49 -0700

    update cypress configuration file

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
+    watchForFileChanges: false,
+    video: false,
+    viewportWidth: 1200,
+    viewportHeight: 800,
+    baseUrl: "https://naveenautomationlabs.com/opencart/index.php?route=common/home",
+    env: {
+      email : '',
+      password: '',
+    },
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
