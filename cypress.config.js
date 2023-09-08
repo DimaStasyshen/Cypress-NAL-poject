981d23797d8afef8a255e5e1b74a5eb6f60905f9
DimaStasyshen <130337129+DimaStasyshen@users.noreply.github.com>
2023-09-07 19:24:23 -0700

    mochawesome reporter update

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
   projectId: "wruxsd",
-  
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     defaultCommandTimeout: 10000,
     watchForFileChanges: false,
-    video: true,
     viewportWidth: 1200,
     viewportHeight: 800,
     baseUrl: "https://naveenautomationlabs.com",
@@ -14,9 +14,18 @@ module.exports = defineConfig({
       email : 'carlosvela@gmail.com',
       password: '12345',
     },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: "custom-title",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
